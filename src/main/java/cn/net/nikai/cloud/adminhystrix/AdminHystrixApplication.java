987082541d85ea8c6510dcf1dac7462d874ec3cb
nikai <keni1006@163.com>
2018-08-01 00:19:16 +0800

    add config file

--- src/main/java/cn/net/nikai/cloud/adminhystrix/AdminHystrixApplication.java
+++ src/main/java/cn/net/nikai/cloud/adminhystrix/AdminHystrixApplication.java
@@ -1,9 +1,13 @@
 package cn.net.nikai.cloud.adminhystrix;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
 public class AdminHystrixApplication {
 
 	public static void main(String[] args) {
