603c4ec2eb5fcbc42fe6d7f9b20dea55f91bd3b0
nikai <keni1006@163.com>
2018-07-31 21:31:51 +0800

    init project

---
+++ src/main/java/cn/net/nikai/cloud/adminhystrix/AdminHystrixApplication.java
@@ -0,0 +1,12 @@
+package cn.net.nikai.cloud.adminhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminHystrixApplication.class, args);
+	}
+}
